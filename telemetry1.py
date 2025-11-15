a0fde3bbd0dfa21f7eb7e145bd200c16dc58ad11
Marian Jandl <marijandlik@gmail.com>
2025-11-15 22:13:09 +0100

    code cleaning

--- telemetry1.py
+++ telemetry1.py
@@ -8,41 +8,15 @@ from textual.widgets import Header, Footer, Static, RichLog, Button, Label, Sele
 from textual.screen import ModalScreen
 from textual.binding import Binding
 
-# -----------------------------
-# Simulated Data Generator
-# -----------------------------
+
 di = 0
 tim = 0
 lineno = 0
 nodata = 0
 
-def generate_data():
-    global di, tim
-    tim += 1
-    if random.randint(0, 4) == 3:
-        a = random.randint(0, 25)
-        if a >= 3 and a <= 7:
-            di = 1
-        elif a >= 7 and a <= 10:
-            di = 3
-        elif a >= 10 and a <= 13:
-            di = 8
-        elif a >= 13 and a <= 15:
-            di = 9
-        elif a >= 15 and a <= 18:
-            di = 11
-        else:
-            di = 0
-    
-    return f"Tim:{tim} Di:{hex(di)} Pwm:0 Vbat:{round(random.random() * 2 + 7, 2)} Iout:{round(random.random() * 20 + 50, 2)} Pout:{round((random.random() * 20 + 50)*(random.random() * 2 + 7)*0.1, 2)} Vfc:{round(random.random() * 2 + 7, 2)} Pfc:{round((random.random() * 20 + 50)*(random.random() * 2 + 7)*0.1, 2)} PfcDes:{round((random.random() * 20 + 50)*(random.random() * 2 + 7)*0.1, 2)} Tfc:{random.randint(40, 80)}"
-
 def get_data(data):
     data = dict(p.split(":") for p in data.split())
     return data
-
-# -----------------------------
-# Connection Manager Screen
-# -----------------------------
 
 class QuitScreen(ModalScreen):
     """Screen with a dialog to quit."""
@@ -235,9 +209,6 @@ class ResourceMonitor(Static):
         except Exception:
             self.update("[dim]Resource monitoring unavailable[/dim]")
 
-# -----------------------------
-# Connection Status Widget
-# -----------------------------
 class ConnectionStatus(Static):
     def __init__(self):
         super().__init__()
@@ -294,9 +265,6 @@ class ErrorStatus(Static):
             else:
                 self.update(f"[bold red]Error code {err_code}: Unknown error - I suppose everything is completly fucked - Good luck[/bold red]")
                             
-# -----------------------------
-# Dashboard Widget
-# -----------------------------
 class Dashboard(Static):
     def __init__(self):
         super().__init__()
@@ -413,9 +381,6 @@ class StatsDashboard(Static):
             stat["count"] = 0
             stat["sum"] = 0
 
-# -----------------------------
-# Main App
-# -----------------------------
 class DashboardLogApp(App):
     CSS = """
     #main_grid {
