9ba2ae6019accd9d0de1c9b8ba9c7089da77cdc1
Marian Jandl <marijandlik@gmail.com>
2025-11-16 23:09:22 +0100

    added some error catching

--- serialcom.py
+++ serialcom.py
@@ -3,32 +3,41 @@ import sys
 import os
 import datetime
 
-
 print("info:Starting serial communication...")
-print("info:port: ", sys.agrv[1], "baudrate: ",sys.argv[2])
-
-ser = serial.Serial(str(sys.argv[1]), int(sys.argv[2]), timeout=1)
+print("info:port: ", sys.argv[1], "baudrate: ",sys.argv[2])
+try:
+    ser = serial.Serial(str(sys.argv[1]), int(sys.argv[2]), timeout=1)
+except Exception as e:
+    print(f"info:{e}")
+    sys.exit(1)
+    
 
 print(f"info:Connected to: {ser.name}")
 
-if not os.path.exists("logs"):
-    os.mkdir("logs")
-
+try:
+    if not os.path.exists("./logs/"):
+        os.mkdir("logs")
+except Exception as e:
+    print(f"info: {e}")
 x = datetime.datetime.now().strftime("%Y%m%d")
 k = 0
 
+print("info:logging test")
 
-while os.path.exists(f"logs/rawdatalog{x}_{k}.txt"):
+while os.path.exists(f"./logs/rawdatalog{x}_{k}.txt"):
     k += 1
-with open(f"logs/rawdatalog{x}_{k}.txt", "a") as f:
+
+with open(f"./logs/rawdatalog{x}_{k}.txt", "a") as f:
     f.write(f"--- New session started at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ---\n")
+
 try:
     while True:
         if ser.in_waiting > 0:
             data = ser.readline().decode('utf-8', errors='ignore').strip()
             if data:
                 print("data:",data)
-                f.write(data + "\n")
+                with open(f"./logs/rawdatalog{x}_{k}.txt", "a") as f:
+                    f.write(data + "\n")
                 sys.stdout.flush()
 except KeyboardInterrupt:   
     print("info:Exiting...")
